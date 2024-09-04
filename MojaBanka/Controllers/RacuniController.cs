77dbb2695b9a1e078fe16c483df8e5d1aee28edd
Marko Punčec <mpuncec46@gmail.com>
2024-09-04 14:39:03 +0200

    Dodavanje View-a

---
+++ MojaBanka/Controllers/RacuniController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MojaBanka.Models;
+
+namespace MojaBanka.Controllers
+{
+    public class RacuniController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+
+        // GET: Racuni
+        public ActionResult Index()
+        {
+            return View(db.Racuni.ToList());
+        }
+
+        // GET: Racuni/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Racun racun = db.Racuni.Find(id);
+            if (racun == null)
+            {
+                return HttpNotFound();
+            }
+            return View(racun);
+        }
+
+        // GET: Racuni/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Racuni/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_racun,Stanje_racun,Id_klijent")] Racun racun)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Racuni.Add(racun);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(racun);
+        }
+
+        // GET: Racuni/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Racun racun = db.Racuni.Find(id);
+            if (racun == null)
+            {
+                return HttpNotFound();
+            }
+            return View(racun);
+        }
+
+        // POST: Racuni/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_racun,Stanje_racun,Id_klijent")] Racun racun)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(racun).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(racun);
+        }
+
+        // GET: Racuni/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Racun racun = db.Racuni.Find(id);
+            if (racun == null)
+            {
+                return HttpNotFound();
+            }
+            return View(racun);
+        }
+
+        // POST: Racuni/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Racun racun = db.Racuni.Find(id);
+            db.Racuni.Remove(racun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MojaBanka/Controllers/TransakcijeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MojaBanka.Models;
+
+namespace MojaBanka.Controllers
+{
+    public class TransakcijeController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+
+        // GET: Transakcije
+        public ActionResult Index()
+        {
+            return View(db.Transakcije.ToList());
+        }
+
+        // GET: Transakcije/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transakcija transakcija = db.Transakcije.Find(id);
+            if (transakcija == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transakcija);
+        }
+
+        // GET: Transakcije/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Transakcije/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_transakcija,Iznos_transakcije,Id_racun")] Transakcija transakcija)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transakcije.Add(transakcija);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(transakcija);
+        }
+
+        // GET: Transakcije/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transakcija transakcija = db.Transakcije.Find(id);
+            if (transakcija == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transakcija);
+        }
+
+        // POST: Transakcije/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_transakcija,Iznos_transakcije,Id_racun")] Transakcija transakcija)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transakcija).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transakcija);
+        }
+
+        // GET: Transakcije/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transakcija transakcija = db.Transakcije.Find(id);
+            if (transakcija == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transakcija);
+        }
+
+        // POST: Transakcije/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Transakcija transakcija = db.Transakcije.Find(id);
+            db.Transakcije.Remove(transakcija);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
