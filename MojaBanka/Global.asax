65a0ddde953499d3abe0ce0058e3ddf35e8c8637
Marko Punčec <mpuncec46@gmail.com>
2024-09-04 02:24:59 +0200

    Initial commit

---
+++ MojaBanka/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MojaBanka.MvcApplication" Language="C#" %>
---
+++ MojaBanka/Models/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace MojaBanka.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name=MyDbContext")
+        {
+        }
+
+        //public DbSet<Klijent> Klijenti { get; set; }
+        //public DbSet<Racun> Racuni { get; set; }
+        //public DbSet<Transakcija> Transakcije { get; set; }
+    }
+}
