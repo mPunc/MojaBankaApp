294f00eff5969f45d9496d7800ff84d6e328010a
Marko Punčec <mpuncec46@gmail.com>
2024-09-10 17:10:39 +0200

    Buttons, ograničen pristup klijenta
    Treba dodati još restrikcije na pojedine metode u kontrolerima

--- MojaBanka/Controllers/HomeController.cs
+++ MojaBanka/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MojaBanka.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,22 +9,11 @@ namespace MojaBanka.Controllers
 {
     public class HomeController : Controller
     {
+        private MyDbContext db = new MyDbContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Baza = db.Klijenti.ToList();
             return View();
         }
     }
